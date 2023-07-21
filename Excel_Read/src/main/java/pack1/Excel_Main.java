88ff89c7994e68a86a8435f6161b923c94b53100
Merin <Merin@DESKTOP-U4FIN6K.bbrouter>
2023-07-21 08:41:03 -0700

    commit

--- Excel_Read/src/main/java/pack1/Excel_Main.java
+++ Excel_Read/src/main/java/pack1/Excel_Main.java
@@ -5,7 +5,7 @@ public class Excel_Main
 {
 public static void main(String[] args) throws IOException
 {
-String s=Excel_new.getStringData(0, 1);
+String s=Excel_new.getStringData(0, 1);  //changed
 System.out.println(s);
 String n=Excel_new.getIntegerData(0,0);
 System.out.println(n);
